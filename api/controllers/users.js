c5dfef9ddc21ef937471812f2d811694f15162cd
jppinal <juanpedro.pinar@cloudappi.net>
2018-11-26 01:51:10 +0100

    Changed controller

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -16,6 +16,8 @@ const findById = (userId) => {
   })
 }
 
+const emailRule = /[a-zA-Z0-9.]*@[a-zA-Z0-9]*.[a-zA-Z0-9]*/g;
+
 module.exports = {
   getusers,
   createUsers,
@@ -30,8 +32,8 @@ function getusers(req, res) {
 
 function createUsers(req, res) {
   var user = req.swagger.params.user.value
-  if (!user.name) return res.status(405).end()
-  if (!user.email) return res.status(405).end()
+  if (!user.name || user.name.trim() === "") return res.status(405).end()
+  if (!user.email || emailRule.test(user.email)) return res.status(405).end()
   user.id = 5
   return res.status(201).json(user).end()
 }
@@ -45,8 +47,11 @@ function getusersById(req, res) {
 
 function updateUsersById(req, res) {
   var userId = req.swagger.params.userId.value
+  var user = req.swagger.params.user.value
+  if (!user.name) return res.status(405).end()
+  if (!user.email) return res.status(405).end()
   findById(userId)
-  .then(user => res.json(user))
+  .then(() => res.json(user))
   .catch(err => res.status(err).end())
 }
 
